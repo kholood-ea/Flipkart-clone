9135ef046da6f79846b22732809cd82e55dde58e
kholood-ea <kholood_ea@hotmail.com>
2022-03-21 15:34:42 +0200

    add category routes

--- Backend/src/index.server.js
+++ Backend/src/index.server.js
@@ -1,34 +1,32 @@
-const express = require('express');
-const env = require('dotenv')
-const app=express();
-const bodyParser=require('body-parser')
-const mongoose = require ('mongoose')
+const express = require("express");
+const env = require("dotenv");
+const app = express();
+const mongoose = require("mongoose");
 
 // routes
-const adminRoutes=require('./routes/admin/auth')
-const authRoutes=require('./routes/auth')
-
-
+const adminRoutes = require("./routes/admin/auth");
+const authRoutes = require("./routes/auth");
+const categoryRoutes = require("./routes/category");
 // environment variables
-env.config()
-
+env.config();
 
 //middleware for parsing request body
 // app.use(express.json())
- app.use(bodyParser())
- app.use('/api',authRoutes )
- app.use('/api',adminRoutes )
+app.use(express.json());
+app.use("/api", authRoutes);
+app.use("/api", adminRoutes);
+app.use("/api", categoryRoutes);
 
+mongoose
+  .connect(`${process.env.DBconnection}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("Database connected");
+  });
 
-mongoose.connect(
-    `${process.env.DBconnection}`, 
-    {
-     useNewUrlParser: true,
-     useUnifiedTopology: true,
-     useCreateIndex:true 
-    }).then(()=>{console.log("Database connected")});
-
-
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is running on port ${process.env.PORT}`)
-})
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
