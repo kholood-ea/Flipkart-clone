a828305eff924fc72412338ccf3cd7df6c104341
kholood-ea <kholood_ea@hotmail.com>
2022-03-22 10:35:04 +0200

    product routes initiation

---
+++ Backend/src/routes/product.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+const { createProduct } = require("../controller/product");
+const { adminMiddleware, requireSignin } = require("../common-middleware");
+const shortid = require("shortid");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(path.dirname(__dirname), "uploads"));
+  },
+  filename: (req, file, cb) => {
+    cb(null, shortid.generate() + "_" + file.originalname);
+  },
+});
+const upload = multer({ storage });
+
+router.post(
+  "/product/create",
+  requireSignin,
+  adminMiddleware,
+  upload.array("productPictures"),
+  createProduct
+);
+// router.get("/category/getcategories", getCategories);
+
+module.exports = router;
