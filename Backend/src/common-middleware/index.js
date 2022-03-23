89f74e108268684f6a4bbeeac89efd2650ad19c4
kholood-ea <kholood_ea@hotmail.com>
2022-03-23 10:38:11 +0200

    adding users only middleware

--- Backend/src/common-middleware/index.js
+++ Backend/src/common-middleware/index.js
@@ -10,7 +10,12 @@ exports.requireSignin = (req, res, next) => {
   next();
 };
 
-exports.userMiddleware = (req, res, next) => {};
+exports.userMiddleware = (req, res, next) => {
+  if (req.user.role !== "user") {
+    return res.status(400).json({ message: "Available For Users Only" });
+  }
+  next();
+};
 
 exports.adminMiddleware = (req, res, next) => {
   if (req.user.role !== "admin") {
