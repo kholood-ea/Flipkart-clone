3b57800ab6b68df3c8b6c614bd5e903ef74e3e35
kholood-ea <kholood_ea@hotmail.com>
2022-03-23 10:07:36 +0200

    specifying user role upon signing in

--- Backend/src/controller/admin/auth.js
+++ Backend/src/controller/admin/auth.js
@@ -40,7 +40,7 @@ exports.signin = (req, res) => {
     if (user) {
       if (user.authenticate(req.body.password) && user.role === "admin") {
         const token = jwt.sign(
-          { _id: user._id, role: "admin" },
+          { _id: user._id, role: user.role },
           process.env.JWT_SECRET,
           {
             expiresIn: "1h",
