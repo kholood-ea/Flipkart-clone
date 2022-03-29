9f81ec542056000a2795ff3b4a15b1c78f6411a8
kholood-ea <kholood_ea@hotmail.com>
2022-03-29 21:57:01 +0200

    pass input type as a prop

--- Frontend/admin-app/src/components/UI/Input/index.js
+++ Frontend/admin-app/src/components/UI/Input/index.js
@@ -14,7 +14,7 @@ export default function Input(props) {
     <Form.Group className="mb-3" controlId="formBasicPassword">
       <Form.Label>{props.label}</Form.Label>
       <Form.Control
-        type="text"
+        type={props.type}
         placeholder={props.placeholder}
         value={props.value}
         onChange={props.onChange}
