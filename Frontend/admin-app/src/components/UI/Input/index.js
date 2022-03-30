d318554749285c6df16273c355629cf603ceee94
kholood-ea <kholood_ea@hotmail.com>
2022-03-30 19:03:41 +0200

    remove unrequired property

--- Frontend/admin-app/src/components/UI/Input/index.js
+++ Frontend/admin-app/src/components/UI/Input/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Form } from "react-bootstrap";
 export default function Input(props) {
   return (
-    <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Group className="mb-3">
       <Form.Label>{props.label}</Form.Label>
       <Form.Control
         type={props.type}
