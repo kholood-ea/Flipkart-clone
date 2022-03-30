a4673d71d00df3dd0314d89616fcf6142b35afc0
kholood-ea <kholood_ea@hotmail.com>
2022-03-30 19:04:35 +0200

    home page sidebar initilization

--- Frontend/admin-app/src/screens/Home/index.js
+++ Frontend/admin-app/src/screens/Home/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Layout from "../../components/Layout";
-
+import Sidebar from "./Sidebar";
 export default function Home() {
   return (
     <Layout>
       <h1>Welcome To Admin Dashboard</h1>
+      <Sidebar />
     </Layout>
   );
 }
