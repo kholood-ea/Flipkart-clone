81fdd0f395df7044f8b7a898dcc73df013a25f62
kholood-ea <kholood_ea@hotmail.com>
2022-04-01 11:48:05 +0200

    add signup actions

---
+++ Frontend/admin-app/src/redux/actions/user.actions.js
@@ -0,0 +1,31 @@
+import { userConstants } from "./constants";
+import axiosInstance from "../../helpers/axios";
+
+export const signup = (user) => {
+  // console.log(user);
+  return async (dispatch) => {
+    dispatch({ type: userConstants.SIGNUP_REQUEST });
+
+    const res = await axiosInstance
+      .post("/admin/signup", {
+        ...user,
+      })
+      .catch((err) => {
+        dispatch({
+          type: userConstants.SIGNUP_FAILURE,
+          payload: err,
+        });
+      });
+
+    if (res.status === 201) {
+      const { message } = res.data;
+
+      dispatch({
+        type: userConstants.SIGNUP_SUCCESS,
+        payload: {
+          message,
+        },
+      });
+    }
+  };
+};
