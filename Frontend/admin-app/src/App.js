16526ff45c70b19ca0cde8703f8c664c68d375ba
kholood-ea <kholood_ea@hotmail.com>
2022-04-02 15:08:25 +0200

    adding orders and products routes

--- Frontend/admin-app/src/App.js
+++ Frontend/admin-app/src/App.js
@@ -5,6 +5,9 @@ import { useDispatch, useSelector } from "react-redux";
 import Home from "../src/screens/Home";
 import Signin from "../src/screens/Signin";
 import Signup from "../src/screens/Signup";
+import Products from "../src/screens/Products";
+import Orders from "../src/screens/Orders";
+
 import { isLoggedIn } from "./redux/actions/auth.actions";
 
 import "./App.css";
@@ -27,6 +30,8 @@ function App() {
         <Routes>
           <Route path="/" element={<PrivateRoute />}>
             <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/orders" element={<Orders />} />
           </Route>
           <Route path="/signin" element={<Signin />} />
           <Route path="/signup" element={<Signup />} />
