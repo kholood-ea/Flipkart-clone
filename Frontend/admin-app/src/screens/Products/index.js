02cb8e0c03897a769f018976345320153e0ed90f
kholood-ea <kholood_ea@hotmail.com>
2022-04-02 15:10:38 +0200

    products screen init

---
+++ Frontend/admin-app/src/screens/Products/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../components/Layout";
+import Sidebar from "../../components/Sidebar";
+
+export default function Products() {
+  return (
+    <>
+      <Layout />
+      <Sidebar />
+    </>
+  );
+}
