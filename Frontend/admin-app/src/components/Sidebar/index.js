4b8742d2ae627dc593b69e01c37aa30741eb1d50
kholood-ea <kholood_ea@hotmail.com>
2022-04-02 14:57:06 +0200

    move sidebar to global component

---
+++ Frontend/admin-app/src/components/Sidebar/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Container, Row, Col, Navbar, Nav } from "react-bootstrap";
+
+import "./style.css";
+export default function Sidebar() {
+  return (
+    <Container>
+      <Row>
+        <Col md={2} className="sidebar">
+          <ul>
+            <li>
+              <Nav.Link href="/">Home</Nav.Link>
+            </li>
+            <li>
+              <Nav.Link href="/products">Products</Nav.Link>
+            </li>
+            <li>
+              <Nav.Link href="/orders">Orders</Nav.Link>
+            </li>
+          </ul>
+        </Col>
+        <Col md={10} style={{ marginLeft: "auto" }}>
+          Container
+        </Col>
+      </Row>
+    </Container>
+  );
+}
--- Frontend/admin-app/src/screens/Home/index.js
+++ Frontend/admin-app/src/screens/Home/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "../../components/Layout";
-import Sidebar from "./Sidebar";
+import Sidebar from "../../components/Sidebar";
 export default function Home() {
   return (
     <Layout>
