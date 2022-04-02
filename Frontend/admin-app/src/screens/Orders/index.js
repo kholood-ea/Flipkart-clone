3fe429bad3117d60288c4c7dce8cd953b12a4bef
kholood-ea <kholood_ea@hotmail.com>
2022-04-02 15:10:26 +0200

    orders screen init

---
+++ Frontend/admin-app/src/screens/Orders/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../components/Layout";
+import Sidebar from "../../components/Sidebar";
+
+export default function Orders() {
+  return (
+    <>
+      <Layout />
+      <Sidebar />
+    </>
+  );
+}
